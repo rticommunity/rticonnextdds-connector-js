cfb40979f33f2683bd87ca34af25dfc0ae6fae74
Sam Raeburn <sam@rti.com>
2019-10-16 13:43:34 +0200

    CON-106: Updated web_http example

--- examples/nodejs/web_http/reader_web.js
+++ examples/nodejs/web_http/reader_web.js
@@ -1,42 +1,37 @@
 /******************************************************************************
-* (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.       *
+* (c) 2005-2019 Copyright, Real-Time Innovations.  All rights reserved.       *
 * No duplications, whole or partial, manual or electronic, may be made        *
 * without express written permission.  Any such copies, or revisions thereof, *
 * must display this notice unaltered.                                         *
 * This code contains trade secrets of Real-Time Innovations, Inc.             *
 ******************************************************************************/
+const rti = require('rticonnextdds-connector')
+const path = require('path')
+const http = require('http')
+const fullpath = path.join(__dirname, '/../ShapeExample.xml')
 
-var sleep = require('sleep');
-var http  = require('http');
-var rti   = require('rticonnextdds-connector');
-
-var connector = new rti.Connector("MyParticipantLibrary::Zero",__dirname +"/../ShapeExample.xml");
-var input = connector.getInput("MySubscriber::MySquareReader");
+const connector = new rti.Connector('MyParticipantLibrary::MySubParticipant', fullpath)
+const input = connector.getInput('MySubscriber::MySquareReader')
 
 http.createServer(function (req, res) {
+  if (req.url === '/read') {
+    console.log('read')
+    input.read()
+  } else if (req.url === '/take') {
+    console.log('take')
+    input.take()
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('Click <a href=\'read\'>read</a> or <a href=\'take\'>take</a>')
+    res.end()
+    return
+  }
 
-    if (req.url=="/read") {
-      console.log('read!');
-      input.read();
-    } else if (req.url=="/take") {
-      console.log('take!');
-      input.take();
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("Click <a href='read'>read</a> or <a href='take'>take</a>");
-      res.end();
-      return;
-    }
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  for (const sample of input.samples.validDataIter) {
+    res.write(JSON.stringify(sample.getJson()))
+  }
+  res.end()
+}).listen(7400, '127.0.0.1')
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log(input.samples.getLength());
-    for (i=0; i < input.samples.getLength(); i++) {
-      if (input.infos.isValid(i)) {
-        res.write(JSON.stringify((input.samples.getJSON(i))));
-      }
-    }
-    res.end();
-
-}).listen(7400, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:7400/');
+console.log('Server running at http://127.0.0.1:7400/')
