3db2dd72026b28ee4fe5406cc26ad9885f671c6f
Sam Raeburn <sam@rti.com>
2019-10-18 12:11:30 +0200

    CON-106: Basic maps example

--- examples/nodejs/web_socket/reader_websocket.js
+++ examples/nodejs/web_socket/reader_websocket.js
@@ -12,10 +12,8 @@ const rti = require('rticonnextdds-connector')
 const socketsio = require('socket.io')
 const path = require('path')
 const fullpath = path.join(__dirname, '/../ShapeExample.xml')
-const connector = new rti.Connector('MyParticipantLibrary::MySubParticipant', fullpath)
-const input = connector.getInput('MySubscriber::MySquareReader')
 
-var server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
   if (req.url === '/simple') {
     fs.readFile(path.join(__dirname, '/indexShape.html'), (error, data) => {
       if (error) {
@@ -34,8 +32,8 @@ var server = http.createServer(function (req, res) {
       res.writeHead(200, { 'Content-Type': 'text/html' })
       res.end(data, 'utf-8')
     })
-  } else if (req.url === '/earth') {
-    fs.readFile(path.join(__dirname, '/indexEarth.html'), (error, data) => {
+  } else if (req.url === '/maps') {
+    fs.readFile(path.join(__dirname, '/indexMaps.html'), (error, data) => {
       if (error) {
         console.log('Error: ' + error)
         throw new Error(error)
@@ -45,18 +43,29 @@ var server = http.createServer(function (req, res) {
     })
   } else {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write("Click <a href='simple'>simple</a>, <a href='chart'>D3 chart</a> or <a href='earth'>earth</a>")
+    res.write("Click <a href='simple'>simple</a>, <a href='chart'>D3 chart</a> or <a href='maps'>maps</a>")
     res.end()
   }
 }).listen(7400, '127.0.0.1')
 console.log('Server running at http://127.0.0.1:7400/')
 
-const io = socketsio.listen(server)
+const run = async () => {
+  const connector = new rti.Connector('MyParticipantLibrary::MySubParticipant', fullpath)
+  const input = connector.getInput('MySubscriber::MySquareReader')
+  const io = socketsio.listen(server)
 
-connector.on('on_data_available',
-  function () {
-    input.take()
-    for (const sample of input.samples) {
-      io.sockets.emit('shape', sample.getJson())
+  for (;;) {
+    try {
+      await input.wait()
+      input.take()
+      for (const sample of input.samples.validDataIter) {
+        console.log('reader_websocket.js: emitting: ' + sample.getJson())
+        io.sockets.emit('shape', sample.getJson())
+      }
+    } catch (err) {
+      console.log('Caught err: ' + err)
     }
-  })
+  }
+}
+
+run()
