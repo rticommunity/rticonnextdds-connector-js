11db2cf16c0c2b5ed11a92ab59f70d5a858cb689
Sam Raeburn <sam@rti.com>
2019-10-23 16:21:09 +0200

    CON-106: Re-added simple visualisation

--- examples/nodejs/web_socket/README.md
+++ examples/nodejs/web_socket/README.md
@@ -1,12 +1,11 @@
 # Web socket example
 
 ## Example description
-In this example, we create a simple application which visualises the data DDS data it receives
-by overlaying it on a map.
-`reader_websocket.js` listens at [http://127.0.0.1:7400](http://127.0.0.1:7400) receives
-data of the Shape type (either Circle, Square or Triangle topic) and overlays the data
-on a map. The receives ``shapesize``, ``color``, ``x`` and ``y`` are converted into
-a map feature.
+In this example, we create a simple application which visualises the data DDS data it receives.
+`reader_websocket.js` listens at [http://127.0.0.1:7400](http://127.0.0.1:7400) for requests
+on how it should visualise the data. There are two options:
+* simple: Prints the information about the current shape
+* maps: Prints the received shapes data on a map
 
 ## Dependencies
 This example requires additional packages:
@@ -21,3 +20,6 @@ They can be installed using `npm install socket.io ol`
 publisher.
 * Run the web server `node ./reader_web_socket.js`
 * In a browser, navigate to [http:/localhost:7400](http://127.0.0.1:7400)
+* Select how you would like to visualise the data (simple, chart or maps).
+* Navigating to [http://localhost:7400/simple](http://localhost:7400/simple) will show the simple visualisation and
+[http://localhost:7400/maps](http://localhost:7400/maps) will show the maps visualation.
--- examples/nodejs/web_socket/reader_websocket.js
+++ examples/nodejs/web_socket/reader_websocket.js
@@ -13,17 +13,33 @@ const socketsio = require('socket.io')
 const path = require('path')
 const fullpath = path.join(__dirname, '/../ShapeExample.xml')
 
-// Create the HTTP server (and configure it to serve index.html when requested)
+// Create the HTTP server (and configure it to serve the requested visualisation)
 const server = http.createServer(function (req, res) {
-  fs.readFile(path.join(__dirname, 'index.html'), (error, data) => {
-    if (error) {
-      console.log('Error: ' + error)
-      throw new Error(error)
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.end(data, 'utf-8')
-    }
-  })
+  if (req.url === '/simple') {
+    fs.readFile(path.join(__dirname, 'indexSimple.html'), (error, data) => {
+      if (error) {
+        console.log('Error: ' + error)
+        throw new Error(error)
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data, 'utf-8')
+      }
+    })
+  } else if (req.url === '/maps') {
+    fs.readFile(path.join(__dirname, 'indexMaps.html'), (error, data) => {
+      if (error) {
+        console.log('Error: ' + error)
+        throw new Error(error)
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data, 'utf-8')
+      }
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write("Select your visualisation: <a href='simple'>simple</a> or <a href='maps'>maps</a>")
+    res.end()
+  }
 }).listen(7400, '127.0.0.1')
 console.log('Server running at http://127.0.0.1:7400/')
 
@@ -70,7 +86,11 @@ const run = async () => {
     }
 
     // Take data on each input and emit the corresponding socket event
-    await Promise.all(promisesToWait)
+    try {
+      await Promise.all(promisesToWait)
+    } catch (err) {
+      console.log('Caught err: ' + err)
+    }
   }
 }
 
