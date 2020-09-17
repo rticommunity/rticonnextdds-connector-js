e1a2531117b9883d3840c475ef1322dea0ea0262
Sam Raeburn <sam@rti.com>
2020-09-17 13:42:18 +0200

    CON_173: Correct struct package

--- rticonnextdds-connector.js
+++ rticonnextdds-connector.js
@@ -10,7 +10,7 @@ const os = require('os')
 const ref = require('ref-napi')
 const ffi = require('ffi-napi')
 const path = require('path')
-const StructType = require('ref-napi-struct')
+const StructType = require('ref-struct-napi')
 const EventEmitter = require('events').EventEmitter
 
 /**
