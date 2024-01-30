8eb101641969fef8621cb395b4946d028085e443
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2024-01-30 10:54:51 +0100

    COMMUNITY-76: move Jenkinsfile and Dockerfile

--- resources/jenkins/build_and_test.groovy
+++ resources/jenkins/build_and_test.groovy
@@ -77,6 +77,7 @@ pipeline {
                         agent {
                             dockerfile {
                                 additionalBuildArgs  "--build-arg NODE_VERSION=${NODE_VERSION}"
+                                dir 'resources/docker'
                                 reuseNode true
                             }
                         }
@@ -110,6 +111,7 @@ pipeline {
                             dockerfile {
                                 args '--network none'
                                 additionalBuildArgs  "--build-arg NODE_VERSION=${NODE_VERSION}"
+                                dir 'resources/docker'
                                 reuseNode true
                             }
                         }
