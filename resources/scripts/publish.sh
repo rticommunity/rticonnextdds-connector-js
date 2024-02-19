f9b5c83b45ac8f67b335d3ec8c891bb865b4cae6
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2024-02-19 18:12:46 +0100

    COMMUNITY-80: add -r option to jq

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -2,16 +2,16 @@
 set -x
 
 json_name='package.json'
-package_name=$(jq '.name' $json_name)
-package_version=$(jq '.version' $json_name)
+package_name=$(jq -r '.name' $json_name)
+package_version=$(jq -r '.version' $json_name)
 libraries=(**/lib/*/*.so)
 
-if [ ${#so_files[@]} -eq 0 ]; then
+if [ ${#libraries[@]} -eq 0 ]; then
     echo "Error: No libraries were found, download them before using this script."
     exit 1
 fi
 
-build_id=$(strings ${libs[0]} | grep -o 'BUILD_.*')
+build_id=$(strings ${libraries[0]} | grep -o 'BUILD_.*')
 modified_build_id=${build_id//_/\-}
 version_to_publish=$package_version-$modified_build_id
 
