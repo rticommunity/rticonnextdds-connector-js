3d070deaf128c964c62d9e8233e416f8f43b803a
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2024-02-19 17:44:56 +0100

    COMMUNITY-80: replace _ with - in the buildid x2

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -16,5 +16,6 @@ modified_build_id=${build_id//_/\-}
 version_to_publish=$package_version-$modified_build_id
 
 sed -i "/version/c\  \"version\": \"$version_to_publish\"," ${json_name}
+# Unpublish in case we are uploading the same version again
 npm unpublish ${package_name}@${version_to_publish} --registry https://$NPM_RESGISTRY
 npm publish --registry https://$NPM_RESGISTRY
