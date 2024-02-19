2ff6ff5c1f5c37072a7cf710e98c708ab038cff2
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2024-02-19 17:37:11 +0100

    COMMUNITY-80: add package.json to a variable

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -2,8 +2,8 @@
 set -x
 
 json_name='package.json'
-package_name=$(jq '.name' package.json)
-package_version=$(jq '.version' package.json)
+package_name=$(jq '.name' $json_name)
+package_version=$(jq '.version' $json_name)
 libraries=(**/lib/*/*.so)
 
 if [ ${#so_files[@]} -eq 0 ]; then
@@ -14,6 +14,6 @@ fi
 build_id=$(strings ${libs[0]} | grep -o 'BUILD_.*')
 version_to_publish=$package_version-$build_id
 
-sed -i "/version/c\  \"version\": \"$version_to_publish\"," package.json
+sed -i "/version/c\  \"version\": \"$version_to_publish\"," ${json_name}
 npm unpublish ${package_name}@${version_to_publish} --registry https://$NPM_RESGISTRY
 npm publish --registry https://$NPM_RESGISTRY
