63f5c71a9fa5ea0f2ade09d4ce6fd274db5c2811
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2025-09-26 08:45:22 +0200

    COMMUNITY-99: backport new automation

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -12,18 +12,34 @@ if [ ${#libraries[@]} -eq 0 ]; then
     exit 1
 fi
 
+build_id=$(strings ${libraries[0]} | grep -o 'BUILD_.*')
+jq ". += {\"rti.build-id\": \"${build_id}\"}" package.json > package.json.tmp
+mv package.json.tmp package.json
+
+# Get current git branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# If branch doesn't start with release/connector, update the version
+if [[ ! "$current_branch" =~ ^release/connector ]]; then
+    # Append -dev to the current package version
+    new_version="${package_version}-dev"
+
+    # Update package.json with new version
+    jq ".version = \"${new_version}\"" package.json > package.json.tmp
+    mv package.json.tmp package.json
+
+    package_version=$new_version
+    echo "Updated version to ${package_version} for non-release branch"
+fi
+
 # If publishing to a repository other than the default one, add the Build ID to the
 # version string and unpublish the package in case it was already published to the
 # internal repo.
 if [[ -n "$NPM_REGISTRY" ]]; then
-    build_id=$(strings ${libraries[0]} | grep -o 'BUILD_.*')
-    modified_build_id=${build_id//_/\-}
-    version_to_publish=$package_version-$modified_build_id
     registry_opt="--registry https://$NPM_REGISTRY"
 
-    sed -i "/version/c\  \"version\": \"$version_to_publish\"," ${json_name}
     # Unpublish in case we are uploading the same version again
-    npm unpublish ${package_name}@${version_to_publish} ${registry_opt}
+    npm unpublish ${package_name}@${package_version} ${registry_opt}
 fi
 
 npm publish ${registry_opt}
