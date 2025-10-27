207989d8a8c6d856eae688a5aee1a0e735042cf2
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2025-10-27 11:15:30 +0100

    CON-363: backport CON-340 back to release/connector/1.4.0

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -16,8 +16,12 @@ build_id=$(strings ${libraries[0]} | grep -o 'BUILD_.*')
 jq ". += {\"rti.build-id\": \"${build_id}\"}" package.json > package.json.tmp
 mv package.json.tmp package.json
 
-# Get current git branch
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+# Get current git branch from Jenkins environment variable or fallback to git command
+current_branch=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+
+# Show what the detected brach
+echo "Detected branch: '$current_branch'"
+echo "BRANCH_NAME env var: '${BRANCH_NAME:-not set}'"
 
 # If branch doesn't start with release/connector, update the version
 if [[ ! "$current_branch" =~ ^release/connector ]]; then
@@ -30,6 +34,11 @@ if [[ ! "$current_branch" =~ ^release/connector ]]; then
 
     package_version=$new_version
     echo "Updated version to ${package_version} for non-release branch"
+
+    # Update version in package-lock.json
+    npm i --package-lock-only
+else
+    echo "Release branch detected, keeping original version: $package_version"
 fi
 
 # If publishing to a repository other than the default one, add the Build ID to the
