5e9b8a667bc123b4fe18acf594ab518de987b488
Alejandro Núñez <alejnp@rti.com>
2026-08-24 12:26:53 +0200

    PR Feedback: Rename `spy` consts

--- test/nodejs/test_rticonnextdds_events.js
+++ test/nodejs/test_rticonnextdds_events.js
@@ -45,107 +45,115 @@ describe('Connector EventEmitter tests', () => {
   })
 
   it('Callback should be called when event is emitted', () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     connector.emit('on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
-    connector.removeListener('on_data_available', spy)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
+    connector.removeListener('on_data_available', listenerMock)
   })
 
   it('When no data is written, no event should be emitted', async () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     await new Promise(resolve => setTimeout(resolve, 250))
-    assert.strictEqual(spy.mock.callCount(), 0)
+    assert.strictEqual(listenerMock.mock.callCount(), 0)
   })
 
   it('It should not be possible to register the event listener and have a Promise waiting for data simultaneously', async () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     const err = await connector.wait(500).then(() => null).catch(e => e)
     assert.strictEqual(err.message, 'Can not concurrently wait on the same Connector object')
   })
 
   it('Using .removeAllListeners() should remove all eventListeners', () => {
-    const spy1 = mock.fn()
-    const spy2 = mock.fn()
-    connector.on('on_data_available', spy1)
-    connector.on('on_data_available', spy2)
+    const arrayLikeIterable = { length: 2 }
+    const listenerMocks = Array.from(arrayLikeIterable, () => mock.fn())
+    for (const listenerMock of listenerMocks) {
+      connector.on('on_data_available', listenerMock)
+    }
     assert.strictEqual(connector.listenerCount('on_data_available'), 2)
+
     connector.removeAllListeners('on_data_available')
+
     assert.strictEqual(connector.listenerCount('on_data_available'), 0)
   })
 
   it('Should be possible to re-use a Connector after calling waitForCallbackFinalization', async () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     assert.strictEqual(connector.listenerCount('on_data_available'), 1)
     connector.emit('on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
-    connector.removeListener('on_data_available', spy)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
+    connector.removeListener('on_data_available', listenerMock)
     assert.strictEqual(connector.listenerCount('on_data_available'), 0)
     await connector.waitForCallbackFinalization()
-    connector.on('on_data_available', spy)
+    connector.on('on_data_available', listenerMock)
     assert.strictEqual(connector.listenerCount('on_data_available'), 1)
     connector.emit('on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 2)
+    assert.strictEqual(listenerMock.mock.callCount(), 2)
   })
 
   it('Event should be emitted when data is available on an input', async () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
   })
 
   it('Connector.once() should automatically unregister the callback after data is received', async () => {
-    const spy = mock.fn()
-    connector.once('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.once('on_data_available', listenerMock)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
     assert.strictEqual(connector.listenerCount('on_data_available'), 0)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
   })
 
   it('Should be possible to add multiple callbacks for the same event', async () => {
-    const spy1 = mock.fn()
-    const spy2 = mock.fn()
-    connector.on('on_data_available', spy1)
-    connector.on('on_data_available', spy2)
+    const arrayLikeIterable = { length: 2 }
+    const listenerMocks = Array.from(arrayLikeIterable, () => mock.fn())
+    for (const listenerMock of listenerMocks) {
+      connector.on('on_data_available', listenerMock)
+    }
     assert.strictEqual(connector.listenerCount('on_data_available'), 2)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy1.mock.callCount(), 1)
-    assert.strictEqual(spy2.mock.callCount(), 1)
+    for (const listenerMock of listenerMocks) {
+      assert.strictEqual(listenerMock.mock.callCount(), 1)
+    }
   })
 
   it('Possible to uninstall the eventListener with .off()', async () => {
-    const spy = mock.fn()
-    connector.on('on_data_available', spy)
+    const listenerMock = mock.fn()
+    connector.on('on_data_available', listenerMock)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy.mock.callCount(), 1)
-    connector.removeListener('on_data_available', spy)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
+    connector.removeListener('on_data_available', listenerMock)
     assert.strictEqual(connector.listenerCount('on_data_available'), 0)
     output.write()
     await new Promise(resolve => setTimeout(resolve, 250))
-    assert.strictEqual(spy.mock.callCount(), 1)
+    assert.strictEqual(listenerMock.mock.callCount(), 1)
   })
 
   it('Using .off() should only unregister the supplied callback, if multiple are registered', async () => {
-    const spy1 = mock.fn()
-    const spy2 = mock.fn()
-    connector.on('on_data_available', spy1)
-    connector.on('on_data_available', spy2)
+    const arrayLikeIterable = { length: 2 }
+    const listenerMocks = Array.from(arrayLikeIterable, () => mock.fn())
+    for (const listenerMock of listenerMocks) {
+      connector.on('on_data_available', listenerMock)
+    }
     assert.strictEqual(connector.listenerCount('on_data_available'), 2)
     output.write()
     await events.once(connector, 'on_data_available')
-    assert.strictEqual(spy1.mock.callCount(), 1)
-    assert.strictEqual(spy2.mock.callCount(), 1)
+    for (const listenerMock of listenerMocks) {
+      assert.strictEqual(listenerMock.mock.callCount(), 1)
+    }
+    const [spy1, spy2] = listenerMocks
     connector.removeListener('on_data_available', spy1)
     assert.strictEqual(connector.listenerCount('on_data_available'), 1)
     output.write()
